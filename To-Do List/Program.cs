9e61c9101a64ef79d2f33ba2041659992c3a05e1
Holysmasher <shawnsummers@live.com>
2025-12-11 12:17:16 -0700

    Created Menu and generated methods

--- To-Do List/Program.cs
+++ To-Do List/Program.cs
@@ -1,1 +1,62 @@
-﻿Console.WriteLine("Welcome to the To-Do List Application!");
+﻿
+Console.WriteLine("Welcome to the To-Do List Application!");
+Console.WriteLine("What would you like to do today?");
+Console.WriteLine($@"
+A. Add a new task
+B. View all tasks
+C. Mark a task as completed
+D. Remove a task
+E. Save tasks to file
+F. Quit
+");
+string choice = Console.ReadLine();
+
+switch (choice.Trim().ToUpper())
+{
+    case "A":
+        AddTask();
+        break;
+    case "B":
+        ViewTasks();
+        break;
+    case "C":
+        MarkTaskCompleted();
+        break;
+    case "D":
+        RemoveTask();
+        break;
+    case "E":
+        SaveTasksToFile();
+        break;
+    case "F":
+        Console.WriteLine("Goodbye!");
+        break;
+    default:
+        Console.WriteLine("Invalid choice. Please try again.");
+        break;
+}
+
+void SaveTasksToFile()
+{
+    throw new NotImplementedException();
+}
+
+void RemoveTask()
+{
+    throw new NotImplementedException();
+}
+
+void MarkTaskCompleted()
+{
+    throw new NotImplementedException();
+}
+
+void ViewTasks()
+{
+    throw new NotImplementedException();
+}
+
+void AddTask()
+{
+    throw new NotImplementedException();
+}
