0d38d7c9169a38c5b9177c001d13887b93d734a3
Holysmasher <shawnsummers@live.com>
2025-12-11 12:27:34 -0700

    Added TodoTask Class

--- To-Do List/Program.cs
+++ To-Do List/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Text.Json;
+
+
 Console.WriteLine("Welcome to the To-Do List Application!");
 Console.WriteLine("What would you like to do today?");
 Console.WriteLine($@"
@@ -6,40 +8,34 @@ A. Add a new task
 B. View all tasks
 C. Mark a task as completed
 D. Remove a task
-E. Save tasks to file
-F. Quit
+E. Quit
 ");
-string choice = Console.ReadLine();
+bool programRunning = true;
+do {
+    string choice = Console.ReadLine();
+    switch (choice.Trim().ToUpper())
+    {
+        case "A":
+            AddTask();
+            break;
+        case "B":
+            ViewTasks();
+            break;
+        case "C":
+            MarkTaskCompleted();
+            break;
+        case "D":
+            RemoveTask();
+            break;
+        case "E":
+            Console.WriteLine("Goodbye!");
+            break;
+        default:
+            Console.WriteLine("Invalid choice. Please try again.");
+            break;
+    }
+} while (programRunning);
 
-switch (choice.Trim().ToUpper())
-{
-    case "A":
-        AddTask();
-        break;
-    case "B":
-        ViewTasks();
-        break;
-    case "C":
-        MarkTaskCompleted();
-        break;
-    case "D":
-        RemoveTask();
-        break;
-    case "E":
-        SaveTasksToFile();
-        break;
-    case "F":
-        Console.WriteLine("Goodbye!");
-        break;
-    default:
-        Console.WriteLine("Invalid choice. Please try again.");
-        break;
-}
-
-void SaveTasksToFile()
-{
-    throw new NotImplementedException();
-}
 
 void RemoveTask()
 {
@@ -58,5 +54,5 @@ void ViewTasks()
 
 void AddTask()
 {
-    throw new NotImplementedException();
+
 }
